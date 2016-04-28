7826ebd77f1ee38100a591ab252306a7d4228a18
Xolenno <Xolenno@XOXO>
2016-04-28 12:05:36 -0300

    Anuncios, Acessos, Historico

--- si/anuncios/Anuncios.class.php
+++ si/anuncios/Anuncios.class.php
@@ -2,75 +2,86 @@
 
     namespace si\anuncios;
 
+    use si\APIIntegrada;
     use si\helpers\Cache;
 
     class Anuncios {
 
-	private static $anuncios;
-	protected $extraParans = [];
+        private static $anuncios;
+        protected $extraParans = [];
 
-	/** [] */
-	const PARAN_ORDERBY = 'orderby';
-	const PARAN_SEARCH = 'search';
-	const PARAN_KEYWORDS = 'keywords';
-	const PARAN_DESCONTO = 'desconto';
-	const PARAN_PROMOCAO = 'promocao';
+        /** [] */
+        const PARAN_ORDERBY = 'orderby';
+        const PARAN_SEARCH = 'search';
+        const PARAN_KEYWORDS = 'keywords';
+        const PARAN_DESCONTO = 'desconto';
+        const PARAN_PROMOCAO = 'promocao';
 
-	/**
-	 * Array com a lista de funcionalidades
-	 */
-	const PARAN_FUNCIONALIDADES = 'funcionalidade';
-	const PARAN_CATEGORIAS = 'categoria';
+        /**
+         * Array com a lista de funcionalidades
+         */
+        const PARAN_FUNCIONALIDADES = 'funcionalidade';
+        const PARAN_CATEGORIAS = 'categoria';
 
-	/** @var Cache */
-	private static $cache;
+        /** @var Cache */
+        private static $cache;
 
-	static function __init() {
-	    if (!self::$cache) {
-		self::$cache = new Cache('anuncios.registros', 15);
-		self::$anuncios = self::$cache->getContent() ? : [];
-	    }
-	}
+        static function __init()
+        {
+            if (!self::$cache) {
+                self::$cache = new Cache('anuncios.registros', 15);
+                self::$anuncios = self::$cache->getContent() ? : [];
+            }
+        }
 
-	function busca(array $parans = null, $page = 1, $forPage = 20) {
-	    $anuncios = \si\APIIntegrada::exec('anuncios', \si\APIIntegrada::extend($parans, [
-				'page' => (int) $page,
-				'forpage' => $forPage,
-				    ], $this->extraParans), 15);
+        function busca(array $parans = null, $page = 1, $forPage = 20)
+        {
+            $anuncios = APIIntegrada::exec('anuncios', APIIntegrada::extend($parans, [
+                                'page' => (int) $page,
+                                'forpage' => $forPage,
+                                    ], $this->extraParans), 15);
 
-	    foreach ($anuncios->data as $anuncio) {
-		self::$anuncios[$anuncio->urlamigavel] = $anuncio;
-	    }
+            foreach ($anuncios->data as $anuncio) {
+                self::$anuncios[$anuncio->urlamigavel] = $anuncio;
+            }
 
-	    return $anuncios;
-	}
+            return $anuncios;
+        }
 
-	function detalhes($url) {
+        function detalhes($url)
+        {
 
-	    # Verificando no cache
-	    if (!empty(self::$anuncios[$url])) {
-		return self::$anuncios[$url];
-	    }
+            # Verificando no cache
+            if (!empty(self::$anuncios[$url])) {
+                return self::$anuncios[$url];
+            }
 
-	    # Buscando a notícia
-	    $anuncio = \si\APIIntegrada::exec('anuncios/detalhes', [
-			'urlamigavel' => $url,
-	    ]);
+            # Buscando a notícia
+            $anuncio = APIIntegrada::exec('anuncios/detalhes', [
+                        'urlamigavel' => $url,
+            ]);
 
-	    # Notícia encontrada
-	    if ($anuncio) {
-		self::$anuncios[$anuncio->urlamigavel] = $anuncio;
-		return $anuncio;
-	    }
-	    # Notícia inválida
-	    else {
-		return false;
-	    }
-	}
+            # Notícia encontrada
+            if ($anuncio) {
+                self::$anuncios[$anuncio->urlamigavel] = $anuncio;
+                return $anuncio;
+            }
+            # Notícia inválida
+            else {
+                return false;
+            }
+        }
 
-	function __destruct() {
-	    self::$cache->setContent(self::$anuncios);
-	}
+        static function incAcesso(\stdClass $anuncio)
+        {
+            $spot = $anuncio->spot;
+            APIIntegrada::exec('anuncios/incAcesso', ['spot' => $spot], 0);
+        }
+
+        function __destruct()
+        {
+            self::$cache->setContent(self::$anuncios);
+        }
 
     }
 
