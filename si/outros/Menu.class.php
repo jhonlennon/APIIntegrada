ea2c1ff72e248de80f260958e0b59b325abb7ac1
Xolenno <Xolenno@XOXO>
2016-05-31 09:58:28 -0300

    Menu

--- si/outros/Menu.class.php
+++ si/outros/Menu.class.php
@@ -6,49 +6,33 @@
 
     class Menu {
 
-	private static $menu = [];
-	private static $submenu = [];
-	private static $index = [];
+        private static $index = [];
 
-	public static function getMenu($posicao = 0) {
-	    if (!self::$menu) {
-		$busca = APIIntegrada::exec('menu', ['posicao' => $posicao], 15);
-		self::indexarMenus($busca);
-		foreach ($busca as $i => $v) {
-		    self::$menu[$i] = $v->menu;
-		    self::$submenu[$v->menu->urlamigavel] = $v->submenu;
-		}
-	    }
-	    return self::$menu;
-	}
+        public static function getMenu($posicao = 0)
+        {
+            $menus = APIIntegrada::exec('menu', ['posicao' => $posicao], 15);
+            self::indexarMenus($menus);
+            return $menus;
+        }
 
-	private static function indexarMenus($menus) {
-	    if ($menus) {
-		foreach ($menus as $i => $v) {
-		    $menu = isset($v->menu) ? $v->menu : $v;
-		    $submenu = isset($v->submenu) ? $v->submenu : [];
-		    self::$menu[$i] = $menu;
-		    self::$submenu[$menu->urlamigavel] = $menu;
-		    self::$index[$menu->urlamigavel] = $menu;
-		    self::indexarMenus($submenu);
-		}
-	    }
-	}
+        private static function indexarMenus($menus)
+        {
+            if ($menus) {
+                foreach ($menus as $i => $v) {
+                    $menu = isset($v->menu) ? $v->menu : $v;
+                    $submenu = isset($v->submenu) ? $v->submenu : [];
+                    self::$index[$menu->urlamigavel] = $menu;
+                    self::indexarMenus($submenu);
+                }
+            }
+        }
 
-	public static function getSubMenu($urlMenu) {
-	    self::getMenu();
-	    if (isset(self::$submenu[$urlMenu])) {
-		return self::$submenu[$urlMenu];
-	    } else {
-		return null;
-	    }
-	}
-
-	public static function detalhes($url) {
-	    self::getMenu();
-	    $index = isset(self::$index[$url]) ? self::$index[$url] : null;
-	    return $index;
-	}
+        public static function detalhes($url)
+        {
+            self::getMenu();
+            $menu = isset(self::$index[$url]) ? self::$index[$url] : null;
+            return $menu;
+        }
 
     }
     
